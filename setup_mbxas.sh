e5e48c94d4bf4858aff2ce336f875c0922120c8c
Yufeng Liang <yufengliang@lbl.gov>
2017-03-23 16:57:52 -0700

    Add setup_mbxas.sh
      For setting up many-electron xas calculations

---
+++ setup_mbxas.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+
+ system_in=$1
+ wtime_in=$2
+ nproc_in=$3
+
+ if [ $# -ne 3 ]; then
+   echo "usage: $0 system wall_time nproc"
+   exit
+ fi
+
+ function header {
+   cat > $1 << EOF
+#!/bin/bash
+#SBATCH -p $queue
+#SBATCH -t $wtime
+#SBATCH -e $job.err
+#SBATCH -o $job.out
+#SBATCH -J ${system}.$job
+#SBATCH -n $nproc
+#SBATCH --mail-type=FAIL
+#SBATCH --mail-user=pcbee912@gmail.com
+
+ . \$SLURM_SUBMIT_DIR/Input_Block.in
+ \$SHIRLEY_ROOT/scripts/arvid/XAS_${job}.sh
+
+EOF
+}
+
+ # xas
+
+  queue=regular
+  wtime=$wtime_in
+  job=xas
+  system=$system_in
+  nproc=$nproc_in
+
+  header $job.sh
+
+ # ref
+
+  queue=regular
+  wtime=$wtime_in
+  job=ref
+  system=$system_in
+  nproc=$nproc_in
+
+  header $job.sh
+
+ # ana
+
+  queue=debug
+  wtime=00:30:00
+  job=ana
+  system=$system_in
+  nproc=32
+
+  header $job.sh
+
+ # state
+
+  queue=debug
+  wtime=00:30:00
+  job=state
+  system=$system_in
+  nproc=32
+
+  header $job.sh
+
+ # mbxas
+
+  queue=debug
+  wtime=00:30:00
+  job=mbxas
+  system=$system_in
+  nproc=32
+
+  header $job.sh
